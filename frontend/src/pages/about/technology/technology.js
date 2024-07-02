fa7c939408477bc192053343ceade988e4337831
Shivam <shivamwebroot@gmail.com>
2024-07-02 12:06:30 +0530

    sdfgm

---
+++ frontend/src/pages/about/technology/technology.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./technology.scss";
+
+const Technology = () => {
+  return (
+    <>
+    <div className='head'>Header
+    <div className='ppp'>ppp div
+     
+    </div>
+    </div>
+    </> 
+  )
+}
+export default Technology;
