add553b57083fea8550f4ec8356e2acdc1144b38
22rahullubana <rahulwebroot@gmail.com>
2024-07-02 15:48:20 +0530

    newChanges

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import "./footer.scss";
 
+import Nav from 'react-bootstrap/Nav';
+
 const Footer = () => {
   return (
     <>
     <div className='site-footer'>
-        <div className='container-max'>
-            <div class="row">
-                <div class="col-md-6">
-                    <p>fgfds</p>
+        <div className='fotr-container'>
+            <div className="row justify-content-between">
+                <div className="col-md-6">
+                    <div className="leftColm d-flex">
+                        <div className="fotrAbout">
+                            <h3>About BJS</h3>            
+                            <Nav className="d-block">
+                                <Nav.Link href="#">Our Story</Nav.Link>
+                                <Nav.Link href="#">Our Values</Nav.Link>
+                                <Nav.Link href="#">What Makes BJS Different</Nav.Link>
+                                <Nav.Link href="#">Our Technology</Nav.Link>
+                                <Nav.Link href="#">Charity & Community</Nav.Link>
+                                <Nav.Link href="#">Training Academy</Nav.Link>                                                                
+                            </Nav>                         
+                        </div>
+                        <div className="fotrServices">
+                            <h3>Our Services</h3>
+                            <Nav className="d-block">
+                                <Nav.Link href="#">Two-Man Home Delivery</Nav.Link>
+                                <Nav.Link href="#">BJS Extra</Nav.Link>
+                                <Nav.Link href="#">Haulage</Nav.Link>
+                                <Nav.Link href="#">Specialist Construction Haulage</Nav.Link>                                                             
+                            </Nav>                             
+                        </div>
+                        <div className="fotrInfo">
+                            <h3>Information</h3>
+                            <Nav className="d-block">
+                                <Nav.Link href="#">Careers</Nav.Link>
+                                <Nav.Link href="#">Privacy Policy</Nav.Link>
+                                <Nav.Link href="#">CCTV Policy</Nav.Link>                                                              
+                            </Nav>                             
+                        </div>
+                        <div className="fotrSocial">
+                            <a href="#"><img src={process.env.PUBLIC_URL + "/images/fotrStar.png"} alt="" /></a>
+                            <a href="#"><img src={process.env.PUBLIC_URL + "/images/fotrInsta.png"} alt="" /></a>
+                            <a href="#"><img src={process.env.PUBLIC_URL + "/images/fotrLinkdin.png"} alt="" /></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-md-4">
-                    <p>dfdgghfd</p>
+                <div class="col-md-4 p-0 rightColm text-end">
+                    <p><a href="mailto:hello@bjsfamily.com">hello@bjsfamily.com</a></p>
+                    <a href="#"><button className="headOffice">BJS head office</button></a>
+                    <h4>retire.grow.empire</h4>
+                    <a href="#"><button className="view-map">View on Google Maps</button></a>
+
                 </div>
             </div>
     
