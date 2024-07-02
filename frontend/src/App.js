f6def03e4ba61f7e3366a65445dd4db2474225ea
22rahullubana <rahulwebroot@gmail.com>
2024-07-02 11:47:36 +0530

    mainFooter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+    <div className='head'>Footer
+    <div className='ppp'>ppp div
+    
+    </div>
+    </div>
+    </> 
+  )
+}
+export default Footer;
