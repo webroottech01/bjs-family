cdd38ac90d5537224bb93168f53da7e2b5b2d6a6
Shivam <shivamwebroot@gmail.com>
2024-07-02 19:07:30 +0530

    about us page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import './style.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/header/header';
+import Technology from './pages/about/technology/technology';
+import About from './pages/about/about/about';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <Header /> 
       <BrowserRouter>
       <Routes>
-        
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
       
--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -8,7 +8,7 @@ const Header = () => {
        <div className='container'>
               123
                   </div>
-    </div>
+    </div> 
     </> 
   )
 }
