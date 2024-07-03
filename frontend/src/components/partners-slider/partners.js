b640c4e7a8a6cd2cc3f86b4f2001054140a71231
D Luffy <akashwebroot@gmail.com>
2024-07-03 19:07:18 +0530

    news slider

---
+++ frontend/src/components/partners-slider/partners.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import "./partners.scss";
+import Slider from "react-slick";
+
+
+
+
+
+const Partners = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
+
+  return (
+    <>
+      <Slider {...settings}>
+        <div className='partnersLogo'>
+        <img src='./images/richersounds.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/oliverbonas.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/wayfair.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/tikamoon.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/sweeek.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/richersounds.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/oliverbonas.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/wayfair.png' alt="" className='partnerMainlogo' />
+        </div>
+        <div className='partnersLogo'>
+        <img src='./images/tikamoon.png' alt="" className='partnerMainlogo' />
+        </div>
+      </Slider>
+    </>
+  )
+}
+export default Partners;
--- frontend/src/pages/news/news-page/newspage.js
+++ frontend/src/pages/news/news-page/newspage.js
@@ -1,64 +1,66 @@
 import React from 'react';
 import './newspage.scss';
+import Testimonials from '../../../components/testimonial-slider/testimonials';
+import Partners from '../../../components/partners-slider/partners';
 
-const newsData = [{ 
-  id:1,
-  imageUrl: '/images/constructImg.jpg', // Replace with your image URL
-  heading: 'Construct IT extend partnership with Tarmac Building Products’ Mortar Division',
-  article: 'Article',
-  datePosted: 'Posted: 06/10/24',
-  description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
-  time: '2 min read',
-  articleLink: '#',
-
-},
-{
-  id:2,
-  imageUrl: '/images/news1.jpg', // Replace with your image URL
-  heading: 'From the Ground Up: Construct IT and Wienerberger build on strong foundations with new nationwide 4-year contract award',
-  article: 'Article',
-  datePosted: 'Posted: 06/10/24',
-  description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
-  time: '2 min read',
-  articleLink: '#',
-
-},
-{
-  id:3,
-  imageUrl: '/images/news2.jpg', // Replace with your image URL
-  heading: 'Scurri inks deal with BJS Home Delivery to offer “next-level” two-person delivery service',
-  article: 'Article',
-  datePosted: 'Posted: 06/10/24',
-  description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
-  time: '2 min read',
-  articleLink: '#',
-
-},
-{
-  id:4,
-  imageUrl: '/images/news3.jpg', // Replace with your image URL
-  heading: 'Construct IT take delivery of their next batch of Renault Tippers to expand partnership with FGD Limited',
-  article: 'Article',
-  datePosted: 'Posted: 06/10/24',
-  description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
-  time: '2 min read',
-  articleLink: '#',
-
-},
-{
-  id:5,
-  imageUrl: '/images/news4.jpg', // Replace with your image URL
-  heading: 'Construct IT and Wienerberger launch from new PD Ports Teesport site',
-  article: 'Article',
-  datePosted: 'Posted: 06/10/24',
-  description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
-  time: '2 min read',
-  articleLink: '#',
-
-},
+const newsData = [
+  {
+    id: 1,
+    imageUrl: '/images/constructImg.jpg',
+    heading: 'Construct IT extend partnership with Tarmac Building Products’ Mortar Division',
+    article: 'Article',
+    datePosted: 'Posted: 06/10/24',
+    description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
+    time: '2 min read',
+    articleLink: '#',
+  },
+  {
+    id: 2,
+    imageUrl: '/images/news1.jpg',
+    heading: 'From the Ground Up: Construct IT and Wienerberger build on strong foundations with new nationwide 4-year contract award',
+    article: 'Article',
+    datePosted: 'Posted: 06/10/24',
+    description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
+    time: '2 min read',
+    articleLink: '#',
+  },
+  {
+    id: 3,
+    imageUrl: '/images/news2.jpg',
+    heading: 'Scurri inks deal with BJS Home Delivery to offer “next-level” two-person delivery service',
+    article: 'Article',
+    datePosted: 'Posted: 06/10/24',
+    description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
+    time: '2 min read',
+    articleLink: '#',
+  },
+  {
+    id: 4,
+    imageUrl: '/images/news3.jpg',
+    heading: 'Construct IT take delivery of their next batch of Renault Tippers to expand partnership with FGD Limited',
+    article: 'Article',
+    datePosted: 'Posted: 06/10/24',
+    description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
+    time: '2 min read',
+    articleLink: '#',
+  },
+  {
+    id: 5,
+    imageUrl: '/images/news4.jpg',
+    heading: 'Construct IT and Wienerberger launch from new PD Ports Teesport site',
+    article: 'Article',
+    datePosted: 'Posted: 06/10/24',
+    description: 'Nationwide construction haulier Construct IT are thrilled to announce a new strategic partnership with Tarmac Building Products Mortar Division, delivering their bagged mortar nationwide...',
+    time: '2 min read',
+    articleLink: '#',
+  },
 ];
 
 const Newspage = () => {
+  const viewedStories = 4; // Static value for viewed stories
+  const totalStories = newsData.length;
+  const percentage = (viewedStories / totalStories) * 100;
+
   return (
     <div className='newsMain'>
       <div className='container imageSection'>
@@ -85,18 +87,18 @@ const Newspage = () => {
               </div>
             </div>
           </div>
-          
+
           {/* Remaining four news items */}
           <div className='col-md-6 listNews'>
-            {newsData.slice(1, 5).map((news) => (
+            {newsData.slice(1).map((news) => (
               <div key={news.id} className='singleNews d-flex'>
                 <img src={process.env.PUBLIC_URL + news.imageUrl} alt="" />
                 <div className='mainImgContent'>
-                      <h3 className='text-start'>{news.heading}</h3>
-                    <div className='d-flex justify-content-start gap-3 article'>
-                      <p><img src={process.env.PUBLIC_URL + "/images/articleIcon.png"} alt="" className='iconMain' /> {news.article}</p>
-                      <p>{news.datePosted}</p>
-                    </div>
+                  <h3 className='text-start'>{news.heading}</h3>
+                  <div className='d-flex justify-content-start gap-3 article'>
+                    <p><img src={process.env.PUBLIC_URL + "/images/articleIcon.png"} alt="" className='iconMain' /> {news.article}</p>
+                    <p>{news.datePosted}</p>
+                  </div>
                   <div className='d-flex justify-content-between timeLink'>
                     <p><img src={process.env.PUBLIC_URL + "/images/clock.png"} alt="" className='iconMain' /> {news.time}</p>
                     <a href={news.articleLink}>Read Article <img src={process.env.PUBLIC_URL + "/images/nextArrow.png"} alt="" className='iconMain' /> </a>
@@ -105,19 +107,39 @@ const Newspage = () => {
               </div>
             ))}
           </div>
-          
+
         </div>
       </div>
       <div className='totalStories'>
-      <p>You have viewed 5 out of {newsData.length} stories</p>
+        <p>You have viewed {viewedStories} out of {totalStories} stories</p>
+        <div className="progress-bar">
+          <div className="fill" style={{ width: `${percentage}%` }}></div>
+        </div>
+        <a href='#'>VIEW ALL STORIES</a>
       </div>
 
       <div className='mediaSlider'>
         <h2 className='text-center'>MENTIONED IN THE MEDIA</h2>
       </div>
+
+      {/* TESTImonial Carousel */}
+      <div className='container newstestimonial'>
+        <h2 className='text-start'>what our customers think</h2>
+        <Testimonials />
+      </div>
+
+      {/* TESTImonial Carousel */}
+      <div className='partnersMAin'>
+      <div className='newsPartners'>
+        <h2 className='text-center'>Partners</h2>
+        <Partners />
+      </div>
+      </div>
+
+
+
     </div>
   );
 };
-
 
 export default Newspage;
